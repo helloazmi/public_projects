583bd3a935ab3b5000e8d4c2c4072dbce0674f63
hellophant <azmi.akash@gmail.com>
2020-12-20 19:56:40 +0100

    sdg

---
+++ dashboard_pub/js_version/script.js
@@ -0,0 +1,29 @@
+'use strict';
+
+// Row
+
+// Item meaning button
+
+// Content of the tile/button
+
+console.log("script.js loaded");
+
+
+
+const gmail = {
+  title: 'Mail',
+  web_url: 'https://gmail.com',
+  img_path: '../img/iconimg_gmail.png'
+}
+
+const gcal = {
+  title: 'Calendar',
+  web_url: 'https://gmail.com',
+  img_path: '../img/iconimg_gcal.png'
+}
+
+const gdrive = {
+  title: 'Drive',
+  web_url: 'https://drive.google.com',
+  img_path: '../img/iconimg_drive.png'
+}
