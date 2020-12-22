47dab55a740859592cb1c1522ae004c60cc877e9
hellophant <azmi.akash@gmail.com>
2020-12-22 21:02:48 +0100

    yui

--- pub_apisl/script.js
+++ pub_apisl/script.js
@@ -11,17 +11,27 @@ API: ec95886bb5ed469d90f9ac4cad56766d
 let search = 'Skanstull';
 let num_results = '5';
 const apiKey_plattsuppslag = '669ff8be5c704ffeaf1b0361259dd5c2';
-let url = `https://api.sl.se/api2/typeahead.json?key=${apiKey_plattsuppslag}&searchstring=${search}=s&maxresults=${num_results}`
+
+let url = `https://api.sl.se/api2/typeahead.json?key=${apiKey_plattsuppslag}&searchstring=${search}=s&maxresults=${num_results}`;
 
 
-const apiKey_realtidsinformation4 = 'ec95886bb5ed469d90f9ac4cad56766d'
-
-
+//const apiKey_realtidsinformation4 = 'ec95886bb5ed469d90f9ac4cad56766d';
 
 
 fetch(url).then(response => {
   console.log(response); //result promise (status code etc)
-  return response.json();
-}).then(response => {
-  console.log(response); // jsonify the response
-}).then(error => console.log('ERROR'));
+  return console.log(response);
+})
+
+
+// fetch(url).then(response => {
+//   console.log(response); //result promise (status code etc)
+//   return response.json();
+// }).then(response => {
+//   console.log(response); // jsonify the response
+// }).then(error => console.log('ERROR'));
+
+
+// fetch(url, {method: "get"})
+//   .then(res => .res.json())
+//   .then(data => console.log(data));
