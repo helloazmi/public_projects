2bdfbefad2b2968d97d7981601cf227592c4bd45
hellophant <azmi.akash@gmail.com>
2020-12-25 13:34:39 +0100

    asd

--- api_sl/script.js
+++ api_sl/script.js
@@ -1,11 +1,5 @@
 console.log('script.js loaded!');
 
-// SL Plattsuppslag - Hjälper att hitta SITE-ID på tågen
-// https://www.trafiklab.se/api/sl-platsuppslag/dokumentation
-// API: 669ff8be5c704ffeaf1b0361259dd5c2
-
-// SL Realtidsinformation 4 - Tabellet för plats A till plats B
-// API: ec95886bb5ed469d90f9ac4cad56766d
 let targetName;
 let targetSiteId;
 
